6da63422b84482f0a44b1a6eb242bf043b20dfba
Tester <k.borschev.exceedteam@gmail.com>
2021-04-28 18:11:38 +0300

    bug fixes

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,10 +1,10 @@
 const Task = require('../models/task.model');
 
 exports.task_create = function (req, res, next) {
+  console.log(req.body, 'req body');
   let task = new Task({
-    task: req.body.task,
+    title: req.body.title,
     isCompleted: req.body.isCompleted,
-    key: req.body.key,
   });
 
   task.save(function (err) {
@@ -19,46 +19,44 @@ exports.task_read = function (req, res, next) {
   Task.find({}, function (err, tasks) {
     if (err) return next(err);
 
-    // tasks = tasks.map(e => {
-    //   return {task: e.task, isCompleted: e.isCompleted, key: e.key};
-    // })
+    tasks = tasks.map((e) => {
+      return { title: e.title, isCompleted: e.isCompleted, id: e._id };
+    });
     res.send(tasks);
   });
 };
 
 exports.task_update = function (req, res, next) {
-  console.log(req.body, 'req body')
-  if (req.body[0].hasOwnProperty('task')) {
-    req.body.forEach(item => {
-        Task.findOneAndUpdate(
-            { key: item.key },
-            { $set: { task: item.task } },
-            function (err) {
-              if (err) return next(err);
-            }
-        );
-    })
+  if (req.body[0].hasOwnProperty('title')) {
+    req.body.forEach((item) => {
+      Task.findByIdAndUpdate(
+        item.id,
+        { $set: { title: item.title } },
+        function (err) {
+          if (err) return next(err);
+        }
+      );
+    });
   }
   if (req.body[0].hasOwnProperty('isCompleted')) {
-    req.body.forEach(item => {
-        Task.findOneAndUpdate(
-            { key: item.key, isCompleted: !item.isCompleted },
-            { $set: { isCompleted: item.isCompleted } },
-            function (err) {
-              if (err) return next(err);
-            }
-        );
-    })
+    req.body.forEach((item) => {
+      Task.findOneAndUpdate(
+        { _id: item.id, isCompleted: !item.isCompleted },
+        { $set: { isCompleted: item.isCompleted } },
+        function (err) {
+          if (err) return next(err);
+        }
+      );
+    });
   }
-  res.send('task updated')
+  res.send('task updated');
 };
 
-exports.task_delete = function (req, res, next) {
-  console.log(req.body)
-    req.body.forEach(item => {
-        Task.findOneAndDelete({ key: item.key }, function (err) {
-        if (err) return next(err);
-        });
-    })
-    res.send('task(s) deleted.');
+exports.task_delete = async function (req, res, next) {
+  await req.body.forEach((item) => {
+    Task.findByIdAndDelete(item.id, function (err) {
+      if (err) return next(err);
+    });
+  });
+  res.send('task(s) deleted.');
 };
--- models/task.model.js
+++ models/task.model.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let TaskSchema = new Schema({
-  task: { type: String, required: true },
+  title: { type: String, required: true },
   isCompleted: { type: Boolean, required: true },
-  key: { type: Number, required: true, unique: true },
 });
 
-// Export the model
 module.exports = mongoose.model('Task', TaskSchema);
