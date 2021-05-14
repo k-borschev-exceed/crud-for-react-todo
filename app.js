2bc51724e57eafd9d2dec845d6428a89c41a6cbd
Tester <k.borschev.exceedteam@gmail.com>
2021-05-14 11:26:36 +0300

    add build in gitignore

--- app.js
+++ app.js
@@ -18,14 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('client/build'));
 app.use('/', express.static(__dirname + '/'));
-
 app.use('/tasks', requireAuth)
 app.use('/tasks',task);
 app.use('/', auth);
 
-
-
-// Express serve up index.html file if it doesn't recognize route
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
